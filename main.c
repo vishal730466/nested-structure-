a69d5bed31abcfbed114c4faf38397841e10c63c
vishal <102140224+vishal730466@users.noreply.github.com>
2022-12-15 13:19:02 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+
+struct address {
+ int pin;
+ char city[10];
+};
+struct emp{
+   char a;
+   int id;
+   struct address a1;
+}s1;
+
+int main(void) {
+ int i,id;
+  char a,city[20];
+  
+printf("enter alphabet, id and pin ");
+scanf("%c%d%d",&s1.a,&s1.id,&s1.a1.pin);
+  
+  printf("enter city ");
+  scanf("%s",&city);
+
+ strcpy(s1.a1.city,city);
+  
+    printf(" your id is %d\n your code is %c\n your pin is %d \n you city is %s",s1.id,s1.a,s1.a1.pin,s1.a1.city);       
+}
